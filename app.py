a6f245d995fd36739b124b85e7f9e7fc13667778
Deepshikha Keshri <deepshikhakeshri2403@gmail.com>
2025-04-21 11:24:07 +0530

    requirement

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ model = joblib.load('model.pkl')
 st.title("Customer Churn Prediction")
 
 
+print(type(model))
+
+
+
 credit_score = st.number_input("Credit Score", min_value=300, max_value=850, value=650)
 age = st.number_input("Age", min_value=18, max_value=100, value=30)
 balance = st.number_input("Balance", min_value=0.0, value=50000.0)
